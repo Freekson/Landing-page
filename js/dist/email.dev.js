81f21bd28619a004d8c8d9557af885e310fee445
Freekson <79405864+Freekson@users.noreply.github.com>
2021-02-21 14:05:32 +0100

    Add files via upload

---
+++ js/dist/email.dev.js
@@ -0,0 +1,58 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.send_form').on('click', function () {
+    var name = $('input[name = name]').val();
+    var sorname = $('input[name = sorname]').val();
+    var target = $('input[name = target]').val();
+    var illnes = $('input[name = illnes]').val();
+    var studies = $('input[name = studies]').val();
+    var mail = $('input[name = mail]').val();
+    var information = $('textarea[name = information]').val();
+
+    if (name == '') {
+      $('input[name = name]').css('border-color', 'rgb(226, 0, 26)');
+      $('input[name = name]').focus(function () {
+        $(this).css('border-color', 'rgb(220, 220, 220)');
+      });
+      return false;
+    }
+
+    if (sorname == '') {
+      $('input[name = sorname]').css('border-color', 'rgb(226, 0, 26)');
+      $('input[name = sorname]').focus(function () {
+        $(this).css('border-color', 'rgb(220, 220, 220)');
+      });
+      return false;
+    }
+
+    if (target == '') {
+      $('input[name = target]').css('border-color', 'rgb(226, 0, 26)');
+      $('input[name = target]').focus(function () {
+        $(this).css('border-color', 'rgb(220, 220, 220)');
+      });
+      return false;
+    }
+
+    if (mail == '') {
+      $('input[name = mail]').css('border-color', 'rgb(226, 0, 26)');
+      $('input[name = mail]').focus(function () {
+        $(this).css('border-color', 'rgb(220, 220, 220)');
+      });
+      return false;
+    }
+
+    var data_form = 'imie=' + name + '$sor=' + sorname + '$target=' + target + '$illnes=' + illnes + '$studies=' + studies + '$mail=' + mail + '$information' + information;
+    $('.server_answer').css('display', 'block');
+    $.ajax({
+      type: "POST",
+      url: "data.php",
+      data: data_form,
+      success: function success() {},
+      error: function error() {
+        $('.server_answer').html("<p class='error'>Wystąpiły problemy techniczne. Proszę sprobować póżniej</p>");
+        $('.server_answer').delay(3000).slideUp(500);
+      }
+    });
+  });
+});
---
+++ js/dist/navigation.dev.js
@@ -0,0 +1,88 @@
+"use strict";
+
+$(document).ready(function () {
+  var show_menu = true;
+  $('.main-link').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $('#main').offset().top - 54
+    }, 700);
+
+    if (show_menu == false) {
+      $('.main-menu').animate({
+        top: -250
+      }, 700);
+      show_menu = true;
+    }
+  });
+  $('.offer-link').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $('#offer').offset().top - 54
+    }, 700);
+
+    if (show_menu == false) {
+      $('.main-menu').animate({
+        top: -250
+      }, 700);
+      show_menu = true;
+    }
+  });
+  $('.diet-link').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $('#diet').offset().top - 54
+    }, 700);
+
+    if (show_menu == false) {
+      $('.main-menu').animate({
+        top: -250
+      }, 700);
+      show_menu = true;
+    }
+  });
+  $('.contact-link').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $('#contact').offset().top - 54
+    }, 700);
+
+    if (show_menu == false) {
+      $('.main-menu').animate({
+        top: -250
+      }, 700);
+      show_menu = true;
+    }
+  });
+  $('.creator-link').on('click', function () {
+    $('html, body').animate({
+      scrollTop: $('#creator').offset().top - 54
+    }, 700);
+
+    if (show_menu == false) {
+      $('.main-menu').animate({
+        top: -250
+      }, 700);
+      show_menu = true;
+    }
+  });
+  $('.menu-visible').on('click', function () {
+    if (show_menu == true) {
+      $('.main-menu').animate({
+        top: 0
+      }, 700);
+      show_menu = false;
+    } else {
+      $('.main-menu').animate({
+        top: -250
+      }, 700);
+      show_menu = true;
+    }
+  });
+  $(window).resize(function () {
+    var window_width = $(window).width();
+
+    if (window_width > 768) {
+      $('.main-menu').css('top', 0);
+      show_menu = true;
+    } else {
+      $('.main-menu').css('top', -250);
+    }
+  });
+});
